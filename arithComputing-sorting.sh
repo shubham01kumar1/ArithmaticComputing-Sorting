af47d3077ccd3dba23445bfbf636f7708057a4a7
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-07-18 23:04:39 +0530

    storing result from dictionary to array feature

--- arithComputing-sorting.sh
+++ arithComputing-sorting.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 declare -A arith_Computation
+declare -a result_Array
 
 read -p "Enter the value for a: " a
 read -p "Enter the value for b: " b
@@ -10,3 +11,9 @@ arith_Computation["a+b*c"]=$((a + b * c))
 arith_Computation["a*b+c"]=$((a * b + c))
 arith_Computation["c+a/b"]=$((c + a / b))
 arith_Computation["a%b+c"]=$((a % b + c))
+
+counter=0
+for key in ${!arith_Computation[@]}
+do
+	result_Array[((counter++))]=${arith_Computation[$key]}
+done
