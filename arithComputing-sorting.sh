4d775927724b8c7801c18f45c867d7f091e1eadd
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-07-18 23:19:28 +0530

    desc. sorting of result array for arith. computing

--- arithComputing-sorting.sh
+++ arithComputing-sorting.sh
@@ -7,13 +7,39 @@ read -p "Enter the value for a: " a
 read -p "Enter the value for b: " b
 read -p "Enter the value for c: " c
 
+function dsc_Sorting() {
+arr=("$@")
+len=${#arr[@]}
+for ((i=0; i<$len; i++))
+do
+    flag=0
+    for((j=0; j<$len-$i-1; j++))
+    do
+        if [ ${arr[$j]} -lt ${arr[$((j+1))]} ]
+        then
+            temp=${arr[$j]}
+            arr[$j]=${arr[$((j+1))]}
+            arr[$((j+1))]=$temp
+            flag=1
+        fi
+    done
+	if [ $flag -eq 0 ]
+    then
+        break
+    fi
+done
+echo ${arr[@]}
+}
+
 arith_Computation["a+b*c"]=$((a + b * c))
 arith_Computation["a*b+c"]=$((a * b + c))
 arith_Computation["c+a/b"]=$((c + a / b))
 arith_Computation["a%b+c"]=$((a % b + c))
 
 counter=0
-for key in ${!arith_Computation[@]}
+for value in ${arith_Computation[@]}
 do
-	result_Array[((counter++))]=${arith_Computation[$key]}
+	result_Array[((counter++))]=$value
 done
+
+dsc_SortedResult=($(dsc_Sorting "${result_Array[@]}"))
